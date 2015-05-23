cc9f1f76cb54eaf8e6bad0ab8c12b95ef3a2bd9f
twozh <xiaogen-zhang@163.com>
2015-05-23 22:36:50 +0800

    1.0

--- main.js
+++ main.js
@@ -1,14 +1,32 @@
 //backbone app
 
 //model
-var Stage = Backbone.Model.extend({	
+var Stage = Backbone.Model.extend({
 	defaults: {
 		money: 0,
 		payDay: 0
 	}
 });
 
-var Debt = Backbone.Model.extend({	
+var Util = {
+	calcStageLeft: function(firstStageDate, stageNum){
+		var now = new Date();
+		var firstStageDateYear = parseInt(firstStageDate.substr(0, 4));
+		var firstStageDateMonth = parseInt(firstStageDate.substr(5, 2));
+		var firstStageDateDay = firstStageDate.substr(8, 2);
+		var deltaMonth = (now.getFullYear() - firstStageDateYear)*12 +
+						 (now.getMonth() + 1) - firstStageDateMonth;						 
+
+		if (deltaMonth < 0) deltaMonth = 0;
+
+		var stageLeft = stageNum - deltaMonth;
+		if (stageLeft < 0) stageLeft = 0;
+
+		return stageLeft;
+	},
+};
+
+var Debt = Backbone.Model.extend({
 	defaults: {
 		name: '',
 		createTime: 0,
@@ -20,9 +38,15 @@ var Debt = Backbone.Model.extend({
 		stageNum: 0,
 		stagePaid: 0,
 		stageLeft: 0,
-		payDay: 0,
+		stageIsPaid: [],
+		firstStageDate: 0,
 		payMoney: 0,
-		remindDaysBeforePayDay: 3
+		remindDaysBeforePayDay: 3,
+
+	},
+	calcStageLeft: function(){
+		return Util.calcStageLeft(this.attributes.firstStageDate, 
+								  this.attributes.stageNum);
 	},
 });
 
@@ -30,6 +54,9 @@ var Debt = Backbone.Model.extend({
 var Debts = Backbone.Collection.extend({
 	model: Debt,
 	localStorage: new Backbone.LocalStorage("Debt"),
+	comparator: function(m){
+		return m.get('firstStageDate').substr(5);
+	},
 });
 
 debts = new Debts();
@@ -37,41 +64,78 @@ debts = new Debts();
 var AddView = Backbone.View.extend({
 	el:$('#addView'),
 	events: {
-		"submit #addViewForm": 		"submit",
+		"submit #addViewForm"			: 		"submit",
+		"keyup #addViewInFirstStageDate":   	"keyupInFirstStageDate",
+		"blur #addViewInFirstStageDate" : 		"keyupInFirstStageDate",
+		"focus #addViewInName"			:       "focusThenClearMsg",
+		"focus #addViewInFirstStageDate":       "focusThenClearMsg",
+		"focus #addViewInStageNum"		:       "focusThenClearMsg",
+		"focus #addViewInPayMoney"		:       "focusThenClearMsg",
 		"click #addViewBtnBack":    function(){
 			this.$el.hide();
 			showView.$el.show();
 		},
 	},
 	initialize: function() {
-		
-		
+
+
 	},
 	render: function() {
 		debts.each(function(d){
 			console.log(d);
-		});		
+		});
 	},
 	submit: function(){
+		if (false === $('#addViewInFirstStageDate').data('valid')){
+			$('#addViewPMsg').html("ri qi ge shi bu dui");
+			return false;
+		}
+
 		//debts
 		var data = {
 			name: $('#addViewInName').val(),
 			createTime: Date(),
-			payDay: $('#addViewInPayDay').val(),
-			payMoney: $('#addViewInPayMoney').val(),
+			firstStageDate: $('#addViewInFirstStageDate').val(),
+			stageNum: parseInt($('#addViewInStageNum').val()),
+			payMoney: parseFloat($('#addViewInPayMoney').val()),
 		};
 
+		var stageLeft = Util.calcStageLeft(data.firstStageDate, data.stageNum);
+		if (stageLeft <= 0){
+			$('#addViewPMsg').html("Have already paided!");
+			return false;
+		}
+
+		data.stageIsPaid = new Array(parseInt(data.stageNum));
+		data.stageLeft = stageLeft;
+		data.stagePaid = data.stageNum - stageLeft;
+
 		var id = $("#addViewBtnSubmit").data('id');
-		
+
 		if (id){
-			debts.get(id).save(data);
+			debts.get(id).save(data, {wait: true});
 		} else {
-			debts.create(data);		
+			debts.create(data, {wait: true});
 		}
 		showView.$el.show();
 		this.$el.hide();
 
 		return false;
+	},
+	keyupInFirstStageDate: function(){
+		var firstStageDate = $('#addViewInFirstStageDate').val();
+		var regex = /^\d{4}-\d{2}-\d{2}/;		
+		
+		if (regex.test(firstStageDate)){
+			$('#addViewInFirstStageDate').next().removeClass('red');
+			$("#addViewInFirstStageDate").data('valid', true);
+		} else {
+			$('#addViewInFirstStageDate').next().addClass('red');
+			$("#addViewInFirstStageDate").data('valid', false);
+		}
+	},
+	focusThenClearMsg: function(){
+		$('#addViewPMsg').html("");
 	},
 	switchToAddView: function(model){
 		showView.$el.hide();
@@ -79,32 +143,31 @@ var AddView = Backbone.View.extend({
 
 		if (model) {
 			$('#addViewInName').val(model.get('name'));
-			$('#addViewInStageNum').val(model.get('stageNum'));	
-			$('#addViewInStagePaid').val(model.get('stagePaid'));			
-			$('#addViewInPayDay').val(model.get('payDay'));
+			$('#addViewInStageNum').val(model.get('stageNum'));
+			$('#addViewInFirstStageDate').val(model.get('firstStageDate'));
 			$('#addViewInPayMoney').val(model.get('payMoney'));
 
-			$('#addViewBtnSubmit').html('Save');
+			$('#addViewBtnSubmit').html('保存');
 			$("#addViewBtnSubmit").data('id', model.id);
 		} else {
 			$('#addViewInName').val('');
-			$('#addViewInStageNum').val('');	
-			$('#addViewInStagePaid').val('');		
-			$('#addViewInPayDay').val('');
+			$('#addViewInStageNum').val('');
+			$('#addViewInFirstStageDate').val('');
 			$('#addViewInPayMoney').val('');
 
-			$('#addViewBtnSubmit').html('Add');
-			$("#addViewBtnSubmit").data('id', '');	
-		}		
+			$('#addViewBtnSubmit').html('创建');
+			$("#addViewBtnSubmit").data('id', '');
+		}
 	},
 });
 
 var DebtView = Backbone.View.extend({
-	//tagName: "tr",
+	tagName: "tr",
 	template: _.template($('#debt-template').html()),
 
 	events: {
 		"click button": 	"destroy",
+		"click input":      "clickRadio",
 		"dblclick": 		function(){
 			addView.switchToAddView(this.model);
 		},
@@ -138,15 +201,47 @@ var DebtView = Backbone.View.extend({
 		this.$el.html(this.template(debt));
 		return this;
 	},
+	clickRadio: function(){
+		var v = this.$("input:radio:checked").val();
+		var stageIsPaid = this.model.get('stageIsPaid');
+		var stageLeft = this.model.get('stageLeft');
+
+		if (stageIsPaid[this.model.get('stagePaid')] == 'true' && v == 'true'){
+			return;
+		}
+
+		if (stageIsPaid[this.model.get('stagePaid')] == 'false' && v == 'false'){
+			return;
+		}
+
+		if (!stageIsPaid[this.model.get('stagePaid')] && v == 'false'){
+			return;
+		}
+
+
+		stageIsPaid[this.model.get('stagePaid')] = v;
+
+		if (v == 'true'){
+			stageLeft -= 1;
+		} else {
+			stageLeft += 1;
+		}
+
+		this.model.set('stageLeft', stageLeft);
+		this.model.set('stageIsPaid', stageIsPaid);
+		this.model.save();
+
+		showView.renderCollection();
+	}
 });
 
 var ShowView = Backbone.View.extend({
 	el:$('#showView'),
-	
-	events: {		
+
+	events: {
 		"click button":    function(){
 			addView.switchToAddView();
-		},		
+		},
 	},
 	initialize: function() {
 		this.listenTo(debts, "reset", this.renderCollection);
@@ -155,21 +250,38 @@ var ShowView = Backbone.View.extend({
 		debts.fetch({reset: true});
 	},
 	renderModel: function(d){
+		console.log(d);
 		var view = new DebtView({model: d});
+		
 		$('#showViewList').prepend(view.render().el);
 	},
-	renderCollection: function(){		
-		//this.$el.empty();		
+	renderCollection: function(){
 		if (debts.length === 0) return;
 
 		var t = $("<p></p>");
+		var tPaided = $("<p></p>");
+
+		$('#showViewList').empty();
+		$('#showViewPaidedList').empty();
 
 		debts.each(function(d){
+			var calcLeft = Util.calcStageLeft(d.get('firstStageDate'), d.get('stageNum'));
+			
+			d.set('stagePaid', d.get('stageNum') - calcLeft);
+			d.save();
+
 			var view = new DebtView({model: d});
-			t.append(view.render().el);
+
+			if ('true' == d.get('stageIsPaid')[d.get('stageNum') - calcLeft]){				
+				tPaided.append(view.render().el);
+			} else {
+				if (!d.get('stageLeft')) return;
+				t.append(view.render().el);	
+			}
 		});
 
 		$('#showViewList').append(t.children());
+		$('#showViewPaidedList').append(tPaided.children());
 	},
 });
 
